79f638ee8195b53bb1a5ad84aecdf004b47b63aa
Gabriel Labriaga <goldengraphixstudios@gmail.com>
2026-01-11 19:54:27 +0800

    Adjust sample works button position - moved left

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1136,8 +1136,8 @@ export default function Portfolio() {
         /* SAMPLE WORKS BUTTON - RIGHT SIDE ABOVE STATS */
         .sample-works-wrapper {
           position: absolute;
-          right: 5%;
-          bottom: 400px;
+          right: 15%;
+          bottom: 500px;
           z-index: 4;
           animation: fadeInUp 1s ease-out 0.9s both;
         }
